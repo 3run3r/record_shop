ff5531a6aa131023b5410e8dae8325afb944ded6
Matteo Fusillo <matt.fusillo@gmail.com>
2020-02-10 17:23:17 +0000

    set up files for hosting on heroku

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,13 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'record_shop', host: 'localhost' })
+      db = PG.connect({ 
+        dbname: 'ddedkkl32u3832', 
+        host: 'ec2-35-172-85-250.compute-1.amazonaws.com',
+        port: 5432,
+        user: 'vjslxzoxvkmfdz', 
+        password: '348d538c22e8681559d1598b1d205042d9c9ac42f37429ff052ff277acfd12c4'
+        })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
